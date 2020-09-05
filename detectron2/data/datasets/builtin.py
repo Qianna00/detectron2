9981c192f7699a3137fcd71f97d1b6ca65887399
Qianna <zhq9669@gmail.com>
2020-09-05 16:17:00 +0800

    none

--- detectron2/data/datasets/builtin.py
+++ detectron2/data/datasets/builtin.py
@@ -219,7 +219,8 @@ def register_all_pascal_voc(root):
 
 
 # Register them all under "./datasets"
-_root = os.getenv("DETECTRON2_DATASETS", "datasets")
+# _root = os.getenv("DETECTRON2_DATASETS", "datasets")
+_root = "/root/data/zq/data"
 register_all_coco(_root)
 register_all_lvis(_root)
 register_all_cityscapes(_root)
